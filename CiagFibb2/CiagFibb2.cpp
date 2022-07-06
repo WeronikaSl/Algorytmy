7f542f9bb7a41726dd82747326d965950732bf68
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-07-06 14:47:53 -0700

    First commit

---
+++ CiagFibb2/CiagFibb2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Fibbonacci
+{
+public:
+    int generate(int n)
+    {
+        if (n <= 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return generate(n - 2) + generate(n - 1);
+        }
+    }
+};
+
+int main()
+{
+    Fibbonacci fib;
+    std::cout << fib.generate(20);
+}
+
